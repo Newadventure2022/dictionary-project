a0c1da9d32d05e3957f1c4da8502cbb9dc59cb57
Newadventure2022 <celias.efl.esl.classes@gmail.com>
2023-07-26 19:49:31 +0200

    Created the search form and set up the function

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
         <header className="App-header">
-          <p>Dictionary App</p>
+          <h1>New Dictionary App By Celia</h1>
         </header>
+        <main>
+          <Dictionary />
+        </main>
         <footer className="App-footer">
-          <p>
-            This app was made with React{" "}
+          <p className="small">
+            This app was made by Celia Corona-Doran with React{" "}
             <img src={logo} className="App-logo" alt="logo" />
           </p>
         </footer>
