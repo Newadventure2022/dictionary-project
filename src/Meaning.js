1a50e046931e355c3eb0716bbb6d6b7f94ef7d38
Newadventure2022 <celias.efl.esl.classes@gmail.com>
2023-07-27 20:00:42 +0200

    updated the font size for the results

--- src/Meaning.js
+++ src/Meaning.js
@@ -6,7 +6,10 @@ export default function Meaning(props) {
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
       <p>{props.meaning.definitions[0].definition}</p>
-      <p>{props.meaning.definitions[0].example}</p>
+      <p>
+        <span className="example">Example:</span>{" "}
+        {props.meaning.definitions[0].example}
+      </p>
     </div>
   );
 }
