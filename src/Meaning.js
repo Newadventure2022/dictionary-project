0499395ccd7845c3e08bd5a8d64c4ce4ccb05793
Newadventure2022 <celias.efl.esl.classes@gmail.com>
2023-07-28 15:26:00 +0200

    Update JS

--- src/Meaning.js
+++ src/Meaning.js
@@ -6,6 +6,7 @@ export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
+      <h4>Hellp Beacuse</h4>
 
       {props.meaning.definitions.map(function (definition, index) {
         return (
