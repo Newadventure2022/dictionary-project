dcb019f3f923a8ce34fc51985dfb579969fb8c89
Newadventure2022 <celias.efl.esl.classes@gmail.com>
2023-07-30 22:55:53 +0200

    Updated the phonetics file

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,13 +1,11 @@
 import React from "react";
 import "./Phonetics.css";
 export default function Phonetic(props) {
-  console.log(props.phonetic);
   return (
     <div className="Phonetic">
       <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
         Listen{" "}
       </a>
-
       {props.phonetic.text}
     </div>
   );
