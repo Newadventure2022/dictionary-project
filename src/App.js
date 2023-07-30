d109e305fca71b89c210fa3f3bcc8176f556b212
Newadventure2022 <celias.efl.esl.classes@gmail.com>
2023-07-30 10:23:46 +0200

    Set the H1 in the App.css

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ export default function App() {
             </div>
           </div>
         </header>
+        <br />
         <main>
           <Dictionary defaultKeyword="Dog" />
         </main>
