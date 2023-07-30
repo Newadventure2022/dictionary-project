1eb17cddb0c94e17638f53ec3acf5ee3377d22b2
Newadventure2022 <celias.efl.esl.classes@gmail.com>
2023-07-30 15:35:07 +0200

    updated the css

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -8,15 +8,22 @@ export default function Dictionary(props) {
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
   }
-
+  function handlePexelsResponse(response) {
+    console.log(response);
+  }
   function search() {
     // documentation: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
   }
+  let pexelsApiKey = "Gx7aRfJkC5p1NGi5UNpYW9SaT0N4EMRLHaVYptB28VtqBcHWtlArm1Uc";
+  let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=1`;
+  axios
+    .get(pexelsApiUrl, { headers: { Authorization: `Bearer ${pexelsApiKey}` } })
+    .then(handlePexelsResponse);
 
   function handleSubmit(event) {
     event.preventDefault();
